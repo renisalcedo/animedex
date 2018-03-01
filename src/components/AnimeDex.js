a96741aa7189ae0e40365afa1a5c25229ae9e008
Reynaldo Salcedo <salcedoreynaldo5@gmail.com>
2018-03-01 01:33:10 -0500

    Added axios and used url for making request to anime api

--- src/components/AnimeDex.js
+++ src/components/AnimeDex.js
@@ -1,14 +1,62 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class AnimeDex extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      input: '',
+      anime: {title: '',  synopsis: '', rating: 0, image: ''}
+    }
+
+    this.onChange = this.onChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
   render() {
     return (
       <section className="animedex">
         <div className="container">
-          <h2>Come find me and edit me /AnimeDex</h2>
+          <form onSubmit={this.handleSubmit} >
+            <label>Search</label>
+            <input onChange={this.onChange} type="text" />
+            <button>Submit</button>
+          </form>
+
+          <div className="anime-info">
+          </div>
         </div>
       </section>
     )
+  }
+
+  // Gets the user input and updates the input state
+  onChange(e) {
+    // User input value
+    const value = e.target.value
+
+    this.setState({ input: value })
+  }
+
+  // Will make the request to api as soon as
+  // user submits data
+  async handleSubmit(e) {
+    e.preventDefault()
+
+    // Gets the data for the anime requested
+    const result = await axios.get(`https://kitsu.io/api/edge//anime?filter[text]=${this.state.input}`)
+    const data = result.data.data[0].attributes
+
+    // Updates state with anime obj
+    this.setState({
+      anime: {
+        title: data.titles.en,
+        synopsis: data.synopsis,
+        rating: data.averageRating,
+        image: data.posterImage.medium
+      }
+    })
   }
 }
 
